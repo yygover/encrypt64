283ca20b942ade3846c6d26aa0796d6c52e3d22b
YY <409590749@qq.com>
2023-10-04 21:53:39 +0900

    Add files via upload

--- encrypt.py
+++ encrypt.py
@@ -23,6 +23,11 @@ def filedialog_clicked(entry):
     entry.set(iFilePath)
 
 
+def on_entry_change(*args):
+    new_value = entry4.get()
+    entry5.set(base64.b64decode(new_value).decode().strip())
+
+
 # 実行ボタン押下時の実行関数
 def conductMain():
     dirPath = entry1.get()
@@ -33,10 +38,18 @@ def conductMain():
         if not getfile:
             messagebox.showerror("error", "没有文件")
         else:
+            k = 0
+            count = 0
+            for path in getfile:
+                if os.path.isfile(os.path.join(dirPath, path)):
+                    count += 1
             for i in getfile:
+                pb['value'] = k / count * 100
                 encryptFile(i, dirPath, desPath)
+                k += 1
     if filePath:
         encryptFile(os.path.basename(filePath), os.path.dirname(filePath), desPath)
+        pb['value'] = 100
 
 
 def encryptFile(file, path, des):
@@ -48,7 +61,6 @@ def encryptFile(file, path, des):
     temp = '''7za.exe a -m0=zstd -mx1 -mhe=on -pwoshiyy00 -aoa "''' + ba + '''.zst" "''' + path + '''/''' + file + '''"'''
     if des:
         temp = '''7za.exe a -m0=zstd -mx1 -mhe=on -pwoshiyy00 -aoa "''' + des + '''/''' + ba + '''.zst" "''' + path + '''/''' + file + '''"'''
-    print(ba)
     process = subprocess.Popen(temp, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     exitcode = process.wait()
     if exitcode != 0:
@@ -68,10 +80,18 @@ def conductMain2():
         if not getfile:
             messagebox.showerror("error", "没有文件")
         else:
+            k = 0
+            count = 0
+            for path in getfile:
+                if os.path.isfile(os.path.join(dirPath, path)):
+                    count += 1
             for i in getfile:
+                pb['value'] = k / count * 100
                 decryptFile(i, dirPath, desPath)
+                k += 1
     if filePath:
         decryptFile(os.path.basename(filePath), os.path.dirname(filePath), desPath)
+        pb['value'] = 100
 
 
 def decryptFile(file, path, des):
@@ -96,7 +116,7 @@ if __name__ == "__main__":
     current_dpi = root.winfo_fpixels('1i')
     scale = current_dpi / ORIGINAL_DPI
     width = round(660 * scale)
-    height = round(280 * scale)
+    height = round(400 * scale)
     root.title("加解密")
     root.geometry(f"{width}x{height}")
     font = ttk.font.Font(root, size=14)
@@ -158,7 +178,42 @@ if __name__ == "__main__":
     button2 = ttk.Button(frame5, text="解密", command=conductMain2)
     button2.pack(side=LEFT, padx=10)
 
-    label = ttk.Label(root, text="注意:文件夹或文件任选其一\n无目标文件夹时自动设置为程序所在文件夹")
-    label.grid(row=10, column=1, sticky=W)
+    frame6 = ttk.Frame(root, padding=10)
+    frame6.grid(row=10, column=1)
+
+    label = ttk.Label(frame6, text="进度")
+    label.pack(side=LEFT)
+    pb = ttk.Progressbar(
+        frame6,
+        orient='horizontal',
+        mode='indeterminate',
+        length=400
+    )
+    pb.pack(side=LEFT)
+
+    label = ttk.Label(root,
+                      text="注意:文件夹或文件任选其一\n无目标文件夹时自动设置为程序所在文件夹\n下面是用来解密加密过的文件名的功能\n将加密过的文件名放入加密文件名栏中即可解密")
+    label.grid(row=11, column=1)
+
+    frame7 = ttk.Frame(root, padding=10)
+    frame7.grid(row=12, column=1)
+
+    En64Label = ttk.Label(frame7, text="加密文件名", padding=(5, 2))
+    En64Label.pack(side=LEFT)
+
+    entry4 = StringVar()
+    En64Entry = ttk.Entry(frame7, textvariable=entry4, width=70)
+    En64Entry.pack(side=LEFT)
+    entry4.trace("w", on_entry_change)
+
+    frame8 = ttk.Frame(root, padding=10)
+    frame8.grid(row=13, column=1, sticky=E)
+
+    De64Label = ttk.Label(frame8, text="解密文件名", padding=(5, 2))
+    De64Label.pack(side=LEFT)
+
+    entry5 = StringVar()
+    De64Entry = ttk.Entry(frame8, textvariable=entry5, width=70)
+    De64Entry.pack(side=LEFT)
 
     root.mainloop()
